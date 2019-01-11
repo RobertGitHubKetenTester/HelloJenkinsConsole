bf7398dcfa4395f2ecc1658c1b47ec48b9b7a397
Rob Vermeulen <rcjvermeulen@hotmail.com>
2019-01-11 12:44:03 +0100

    Add project files.

---
+++ HelloJenkinsConsole/Program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloJenkinsConsole
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CreateMessage());
+        }
+
+        public static String CreateMessage()
+        {
+            return "Hello Jenkins";
+        }
+    }
+}
---
+++ HelloJenkinsUnitTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HelloJenkinsConsole;
+
+namespace HelloJenkinsUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestCreateMessage() { 
+        
+            Assert.AreEqual("Hello Jenkins",Program.CreateMessage());
+        }
+    }
+}
