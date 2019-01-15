2af4547b01e13c2d1363f7a405b30a6777d5c9d8
Rob Vermeulen <rcjvermeulen@hotmail.com>
2019-01-15 08:49:42 +0100

    deling door 0 gemaakt voor sonarcube test

--- HelloJenkinsConsole/Program.cs
+++ HelloJenkinsConsole/Program.cs
@@ -11,6 +11,11 @@ namespace HelloJenkinsConsole
         static void Main(string[] args)
         {
             Console.WriteLine(CreateMessage());
+
+            //slechte code gemaakt vanwege sonarcubetest in Jenkins
+            int a = 100;
+            int b = 0;
+            int c = a / b;
         }
 
         public static String CreateMessage()
